615d6f4c40142d771fce196ebd137676ce80b2bb
Pradeep Rajkumar <pradeep.rajkumar@erosnow.com>
2015-07-25 15:28:07 +0530

    Added App icon image. Created separate view controller for Home screen and Maps view controller.

--- TravComp/TravComp/ViewControllers/MapViewController.swift
+++ TravComp/TravComp/ViewControllers/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  TravComp
 //
 //  Created by Pradeep Rajkumar on 25/07/15.
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.initializeViewControllerSettings()
+    }
+
+    func initializeViewControllerSettings () {
+        
         self.title = "TravComp"
+        self.initializeMapView()
+    }
+    
+    func initializeMapView () {
         
         var camera = GMSCameraPosition.cameraWithLatitude(-33.86,
             longitude: 151.20, zoom: 6)
@@ -26,12 +35,21 @@ class ViewController: UIViewController {
         marker.snippet = "Australia"
         marker.map = mapView
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
