90452056b443686b3e7230a447ba88e0161bcaee
Pradeep Rajkumar <pradeep.rajkumar@erosnow.com>
2015-07-25 14:14:11 +0530

    Basic project + Google Maps sdk added in Pods.

---
+++ TravComp/TravComp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravComp
+//
+//  Created by Pradeep Rajkumar on 25/07/15.
+//  Copyright (c) 2015 GIB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "TravComp"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
